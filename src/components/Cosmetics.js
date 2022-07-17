aa8b5b9bae311c8bcd0ebf2dd106e0ccb528a095
Shaharina Mita <S.mita90232@gmail.com>
2022-07-17 12:03:43 +0600

    style added

--- src/components/Cosmetics.js
+++ src/components/Cosmetics.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cosmetic from './Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
+    const [cosmetics, setCosmetics] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setCosmetics(data));
+    }, []);
+
     // const cosmetics = [
     //     {
     //         id: '62d12b462d51960d0a2fdb02',
